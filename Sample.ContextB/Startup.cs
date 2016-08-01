491d3ac443b971bccbebc9c5ff577c08142bb6a9
Jason Mitchell <jasonmitchell@live.co.uk>
2016-08-01 20:21:02 +0100

    Demo code

---
+++ Sample.ContextB/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace Sample.ContextB
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(new Func<AppFunc, AppFunc>(next => (async env =>
+            {
+                var context = new OwinContext(env);
+                await context.Response.WriteAsync("Hello, ContextB");
+            })));
+        }
+    }
+}
---
+++ Sample.Host/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Sample.Host.Startup))]
+namespace Sample.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var contextA = new ContextA.Startup();
+            var contextB = new ContextB.Startup();
+
+            app.Map("/context-a", contextA.Configuration);
+            app.Map("/context-b", contextB.Configuration);
+        }
+    }
+}
